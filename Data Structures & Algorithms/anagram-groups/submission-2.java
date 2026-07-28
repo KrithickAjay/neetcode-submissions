f8d7fb6104361b6ab24d6edc8cb5608d7bc63aa2
KrithickAjay <164324988+KrithickAjay@users.noreply.github.com>
2026-07-28 20:20:43 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.java
@@ -0,0 +1,52 @@
+// class Solution {
+//     public List<List<String>> groupAnagrams(String[] strs) {
+        
+//         Map<String ,List<String>> map =new HashMap<>();
+//         for(String str : strs){
+
+//             char[] charStr=str.toCharArray();
+//             Arrays.sort(charStr);
+//             String sortStr=new String(charStr);
+//             if(map.containsKey(sortStr)){
+//                 map.get(sortStr).add(str);
+//             }else{
+//                 List<String> list=new ArrayList<>();
+//                 list.add(str);
+//                 map.put(sortStr,list);
+//             }
+//         }
+//         return new ArrayList<>(map.values());
+//     }
+// }
+
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> map =new HashMap<>();
+        for(String str : strs){
+            
+            String freqString = calculateFreqString(str);
+            if(map.containsKey(freqString)){
+                map.get(freqString).add(str);
+            }else{
+                List<String> list=new ArrayList<>();
+                list.add(str);
+                map.put(freqString,list);
+            }
+        }
+            return new ArrayList<>(map.values());
+    }
+    public String calculateFreqString(String str){
+        int[] freq=new int[26];
+        StringBuilder sb=new StringBuilder();
+        for(char ch : str.toCharArray()){
+            freq[ch - 'a']++;
+        }
+        char ch = 'a';
+        for(int i : freq){
+            sb.append(i).append(ch++);
+        }
+        return sb.toString();
+    }
+
+}
