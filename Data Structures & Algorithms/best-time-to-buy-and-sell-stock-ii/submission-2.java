7f9cadef1f519531870bfc9bcfeade12f44ee6af
KrithickAjay <164324988+KrithickAjay@users.noreply.github.com>
2026-09-02 19:56:45 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-2

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] arr) {
+        
+        int max = 0;
+
+        for(int i = 0;i<arr.length-1;i++){
+            if(arr[i] < arr[i+1]){
+                max += arr[i+1]-arr[i];
+            }
+        }
+
+        return max;
+    }
+}
